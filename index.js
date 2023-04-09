c6f4f6c876b24cd6d7e6a8134bfc1097db098161
giladdanino <gilad.danino@ridewithvia.com>
2023-04-09 12:05:47 +0300

    added IMDb top games endpoint

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const cheerio = require('cheerio')
 const app = express()
 const router = express.Router()
 
+app.get('/api/imdb/topgames', async(req, res) => {
+    const response = await axios.get(consts.IMDB_TOP_GAMES_LIST_URL)
+    const html = response.data
+    const $ = cheerio.load(html)
+    const topGamesList = []
+    $('#main h3.lister-item-header a', html).map((i, elem) => {
+        const title = elem.children[0].data
+        topGamesList.push({
+            "title": title,
+            "rank": i+1
+        })
+    })
+    res.send(topGamesList)
+})
+
 app.listen(1111, () => {
     console.log('listening on port 1111');
 })
