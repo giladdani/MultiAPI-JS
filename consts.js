08c91c2fdbd463e0a8103911749b7e9080c281bd
giladdanino <gilad.danino@ridewithvia.com>
2023-04-09 12:03:44 +0300

    initial commit

---
+++ consts.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    IMDB_TOP_GAMES_LIST_URL: 'https://www.imdb.com/list/ls020508641/',
+    METACRITIC_TOP_GAMES_LIST_URL: 'https://www.metacritic.com/browse/games/score/metascore/all/all/filtered'
+});
